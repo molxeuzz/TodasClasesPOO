9a25a5d48680390dfabf00984104a735d52999d6
Mateo Molina González <89753117+molxeuz@users.noreply.github.com>
2025-02-11 11:15:53 -0500

    Ejemplo de clases, objetos y mostrar

--- model.py
+++ model.py
@@ -1,7 +1,15 @@
 
 class Producto:
+
     def __init__(self, nombre: str, precio: float):
         self.nombre: str = nombre
         self.precio: float = precio
+
     def mostrar_info(self) -> str:
         return f"{self.nombre} - {self.precio}"
+
+p1: Producto = Producto("Arroz", 10000)
+p2: Producto = Producto("Panela", 6000)
+
+print(p1.mostrar_info())
+print(p2.mostrar_info())
