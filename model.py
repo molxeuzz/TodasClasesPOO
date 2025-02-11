2ab014db1be0eba9488bf8b4f9cdf179a58a213f
Mateo Molina González <89753117+molxeuz@users.noreply.github.com>
2025-02-11 11:43:13 -0500

    Agregando Tienda

--- model.py
+++ model.py
@@ -23,11 +23,26 @@ class Cliente:
             carrito_str += producto.mostrar_info() + "\n"
         return carrito_str
 
-    def mostrar_carrito(self) -> float:
+    def calcular_total(self) -> float:
         total: float = 0
         for producto in self.carrito:
             total += producto.precio
         return total
+
+class Tienda:
+
+    def __init__(self, nombre: str):
+        self.nombre: str = nombre
+        self.productos: list[Producto] = []
+
+    def agregar_producto(self, producto: Producto):
+        self.productos.append(producto)
+
+    def mostrar_productos(self) -> str:
+        productos_str: str = ""
+        for producto in self.productos:
+            productos_str += producto.mostrar_info() + "\n"
+        return productos_str
 
 p1: Producto = Producto("Arroz", 10000)
 p2: Producto = Producto("Panela", 6000)
