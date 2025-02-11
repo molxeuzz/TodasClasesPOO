b85253bb40af5a2355868fdf85ac14bc810ee083
Mateo Molina González <89753117+molxeuz@users.noreply.github.com>
2025-02-11 11:35:23 -0500

    Agregando Clientes

--- model.py
+++ model.py
@@ -8,8 +8,30 @@ class Producto:
     def mostrar_info(self) -> str:
         return f"{self.nombre} - {self.precio}"
 
+class Cliente:
+
+    def __init__(self, nombre: str):
+        self.nombre: str = nombre
+        self.carrito: list[Producto] = []
+
+    def agregar_producto(self, producto: Producto):
+        self.carrito.append(producto)
+
+    def mostrar_carrito(self) -> str:
+        carrito_str: str = ""
+        for producto in self.carrito:
+            carrito_str += producto.mostrar_info() + "\n"
+        return carrito_str
+
+    def mostrar_carrito(self) -> float:
+        total: float = 0
+        for producto in self.carrito:
+            total += producto.precio
+        return total
+
 p1: Producto = Producto("Arroz", 10000)
 p2: Producto = Producto("Panela", 6000)
 
 print(p1.mostrar_info())
 print(p2.mostrar_info())
+
